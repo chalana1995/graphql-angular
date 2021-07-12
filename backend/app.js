3704f70861749aff403468e2f17fa0ff8c5c88e5
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-07-12 14:26:04 +0530

    mongo db database connet

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,33 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require("dotenv").config();
 
-const { graphqlHTTP } = require('express-graphql')
+//const { graphqlHTTP } = require('express-graphql')
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
+
+// app.use(
+//     '/graphql',
+//     graphqlHTTP({
+//         schema: //,
+//         rootValue: //,
+//         graphiql: true
+//     })
+// )
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log("DB Connected");
+})
 
 const PORT = process.env.PORT || 5000;
 
