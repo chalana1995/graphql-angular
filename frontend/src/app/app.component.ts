17a9d62d6cd983e21b9c5ec362d9516c99c651a0
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-07-13 18:25:23 +0530

    set apollo client to frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+const GET_QUOTES = gql`
+{
+  quotes {
+    quotes {
+      _id
+      quote
+      author
+    }
+  }
+}
+`
 
 @Component({
   selector: 'app-root',
@@ -7,4 +24,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  quotes: Observable<any>
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+
+  }
 }
